94c7ea805b4d508ddbfa5071b625b7027d4e8dca
soul290502 <soul290502@gmail.com>
2014-03-30 18:14:02 +0800

    sortable deletable li

--- localstorage/localstorage.js
+++ localstorage/localstorage.js
@@ -1,9 +1,20 @@
 (function(){
     "use strict";
     // 插入 <ul> 之 <li> 樣板
-  var $list=$('ul'),
+  var $list=$('ul');
 
-  insertaa = function(content){
+  var save=function(){
+    var data = [];
+    // var ob=$(obj);
+      $list.find('li').each(function(){
+        data.push( $(this).find('span').text() )});
+        localStorage.num = JSON.stringify(data);
+  };
+
+  $list.sortable();
+  $list.on('sortstop',save);
+
+  var insertaa = function(content){
     // 用樣板建立一個新 <li>、放進 <ul> 的前面
     var $li = $('<li><input type="text"><span></span><a class="del">刪除</a></li>');
 
@@ -49,8 +60,11 @@
   // $list.on('click','.del',function(){
   //   // alert("del");
   //   alert($(this).index('ul li'));
-  //   // $(this).parents('li','span').remove();
+  //   $(this).parents('li','span').remove();
+  //   save();
   // });
+
+
   // $('ul .del').click(function(){ alert($(this).index('ul li')};
 
 $('#getval').click(function(){
@@ -67,6 +81,7 @@ $list.on('click','.del',function(){  //可以找index
     alert(index);
 
     $(this).parents('li','span').remove();
+    save();
 });
 
   $('#clear').click(function(){
@@ -85,11 +100,13 @@ $list.on('click','.del',function(){  //可以找index
       $li.find('span').text($li.find('input').val());
       $li.removeClass('editing');
 
-      data = [];
-      $list.find('li').each(function(){
-        data.push( $(this).find('span').text() );
-      });
-     localStorage.num = JSON.stringify(data);
+      // data = [];    //放到save() function 中
+      // $list.find('li').each(function(){
+      //   data.push( $(this).find('span').text() );
+      //   localStorage.num = JSON.stringify(data);
+      save(data);
+      // });
+     
     }
      
 
@@ -104,52 +121,4 @@ $list.on('click','.del',function(){  //可以找index
     });
 
 }());
-// (function(){
-//   "use strict";
 
-//   var
-//   $list = $('ul'),
-
-//   // make an <li> element with specified text
-//   makeItem = function(text){
-//     var $li = $('<li><input type="text"><span></span></li>');
-//     if(text){
-//       $li.find('input').val(text);
-//       $li.find('span').text(text);
-//     }
-//     return $li;
-//   };
-
-//   // load data from localStorage
-//   if(localStorage.list){
-//     $.each(JSON.parse(localStorage.list), function(){
-//       makeItem(this).appendTo($list);
-//     });
-//   }
-
-//   // event handlers
-//   $('button').click(function(){
-//     var $li = makeItem().addClass('editing').prependTo($list);
-//     $li.find('input').focus();
-//   });
-
-//   $list.on('keypress', 'input', function(e){
-//     var $li, data;
-
-//     // when Enter is pressed, e.which === 13
-//     if(e.which === 13){
-
-//       // `this` is the <input>
-//       $li = $(this).parents('li');
-//       $li.find('span').text($li.find('input').val());
-//       $li.removeClass('editing');
-
-//       // save into localStorage
-//       data = [];
-//       $list.find('li').each(function(){
-//         data.push( $(this).find('span').text() );
-//       });
-//       localStorage.list = JSON.stringify(data);
-//     }
-//   });
-// }());
